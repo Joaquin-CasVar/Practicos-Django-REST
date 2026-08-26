978d8be51fce491f003ef6a1e23e9b360ab736b3
Alquita <v.alcarazsgarlatta@itecriocuarto.org.ar>
2026-08-25 21:33:04 -0300

    se agrego los modelos

--- src/autos/views.py
+++ src/autos/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+from .models import Auto
+from .serializers import ArticuloSerializer
